4490ce4015db33e25d69d794a7a73220838a35a5
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-07-09 16:26:53 -0600

    Add beginnings of merchant controlled destruction

--- spec/features/merchant/destroy_spec.rb
+++ spec/features/merchant/destroy_spec.rb
@@ -15,5 +15,20 @@ RSpec.describe 'Destroy Existing Merchant' do
       expect(current_path).to eq('/merchants')
       expect(page).to_not have_content(@brian.name)
     end
+
+    it "there is no button to delete if merchant's items have been ordered" do
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      
+
+    end
   end
 end
+
+# As a visitor
+# If a merchant has items that have been ordered
+# I can not delete that merchant
+# Either:
+# - there is no button visible for me to delete the merchant
+# - if I click on the delete button, I see a flash message indicating that the merchant can not be deleted.
